5abca39c9326d8d819407f114570b8d30d92c630
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-09-12 14:10:51 +0530

    commit

--- LLMs/llm_demo.py
+++ LLMs/llm_demo.py
@@ -1,13 +1,16 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-model = ChatGoogleGenerativeAI(
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-flash",
-    google_api_key="AIzaSyAlU06lSQ8CfPuh6ry0WnJjL6MdYTfRvoE"
+    google_api_key=google_api_key 
 )
 
-result = model.invoke("What is the capital of India")
+result = llm.invoke("What is the capital of India")
 
 print(result)
