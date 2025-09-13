f1bc4f513ccca5ef9d0666a14069b2fd4bd485f6
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-09-13 22:43:23 +0530

    commit

--- prompts/prompt_ui.py
+++ prompts/prompt_ui.py
@@ -1,20 +1,28 @@
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_core.prompts import PromptTemplate,load_prompt
 
-load_dotenv()
-model = ChatOpenAI()
+model = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    google_api_key="AIzaSyAlU06lSQ8CfPuh6ry0WnJjL6MdYTfRvoE"
+)
 
 st.header('Reasearch Tool')
 
-paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+paper_input = st.selectbox( "Select Research Paper Name", 
+                           [
+        "ResNet: Deep Residual Learning for Image Recognition",
+        "GANs: Generative Adversarial Nets",
+        "Vision Transformers (ViT): An Image is Worth 16x16 Words",
+        "AlphaGo: Mastering the Game of Go with Deep Neural Networks and Tree Search"
+    ] )
 
 style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
-template = load_prompt('template.json')
+template = load_prompt('prompts/template.json')
 
 
 
